67a8bd05fd629545a190796461ee77ab4645ba18
Pepe Becker <mail@pepebecker.com>
2023-01-03 16:30:46 +0900

    Improve error handling & add supported-languages option
    Signed-off-by: Pepe Becker <mail@pepebecker.com>

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # TextRecognizer
-A command-line interface for performing text recognition on images.
+TextRecognizer is a command-line interface for performing text recognition on images. <br />
+With this tool, you can easily extract text from images and specify the language of the text to improve the accuracy of the recognition.
 
 ## Install
 ```
@@ -13,10 +14,11 @@ recognize-text [--language <language>] <path>
 ```
 
 ## Arguments
-- `<path>`: The path to the image which contains the text to recognize.
+- `<path>`: The path to the image containing the text to recognize.
 
 ## Options
-- `-l`, `--language <language>`: The language of the text to recognize. Default: en.
+- `--supported-languages`: Show a list of all supported languages.
+- `-l`, `--lang <lang>`: The language of the text to recognize. (default: en)
 - `-h`, `--help`: Show help information.
 
 ## Example
@@ -28,4 +30,28 @@ recognize-text /path/to/image.png
 To recognize text in the same image in Spanish:
 ```bash
 recognize-text --language es /path/to/image.png
+```
+
+To list all supported languages:
+```bash
+recognize-text --supported-languages
+```
+
+The command above will output:
+```
+Supported languages:
+en-US    : English
+fr-FR    : français
+it-IT    : italiano
+de-DE    : Deutsch
+es-ES    : español
+pt-BR    : português
+zh-Hans  : 中文
+zh-Hant  : 中文
+yue-Hans : 粤语
+yue-Hant : 廣東話
+ko-KR    : 한국어
+ja-JP    : 日本語
+ru-RU    : русский
+uk-UA    : українська
 ```
--- Sources/TextRecognizer/TextRecognizer.swift
+++ Sources/TextRecognizer/TextRecognizer.swift
@@ -2,70 +2,108 @@ import ArgumentParser
 import AppKit
 import Vision
 
-struct ValidationError: Error {
-    let message: String
-    init(_ message: String) {
-        self.message = message
+enum MyError: Error, LocalizedError, CustomNSError {
+    case notSupported
+    case missingPath
+    case invalidPath(String)
+    case couldNotLoadImage(String)
+    case couldNotConvertImage
+
+    var errorDescription: String? {
+        switch self {
+        case .notSupported:
+            return "Not supported on this macOS version."
+        case .missingPath:
+            return "Please provide a path to an image."
+        case .invalidPath(let path):
+            return "The path \(path) does not exist."
+        case .couldNotLoadImage(let path):
+            return "Could not load image from path: \(path)"
+        case .couldNotConvertImage:
+            return "Could not convert image to CGImage."
+        }
     }
+
+    var errorCode: Int { 1 }
 }
 
 @main
 struct TextRecognizer: ParsableCommand {
-    @Option(name: .shortAndLong, help: "The language of the text to recognize.")
-    var language: String = "en"
+    static var configuration = CommandConfiguration(
+        commandName: "recognize-text",
+        abstract: "Extract text in target language from an image"
+    )
 
-    @Argument(help: "The image which contains the text to recognize.")
-    var path: String
+    @Flag(name: .customLong("supported-languages"), help: "Show supported languages.")
+    var showSupportedLanguages: Bool = false
 
-    func recognize(image: NSImage, languages: [String], callback: @escaping (Result<[String], Error>) -> Void) {
-        let request = VNRecognizeTextRequest { (request, error) in
-            var recognizedText = [String]()
+    @Option(name: [.short, .customLong("lang")], help: "The language of the text to recognize.")
+    var languages = ["en"]
+
+    @Argument(help: "The path to the image containing the text to recognize.")
+    var path: String?
+
+    func recognize(image: NSImage, languages: [String]) throws -> [String] {
+        guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+            throw MyError.couldNotConvertImage
+        }
+        let semaphore = DispatchSemaphore(value: 0)
+        var result = [String]()
+        var error: Error?
+        let request = VNRecognizeTextRequest { (request, err) in
+            guard err == nil else {
+                error = err
+                return
+            }
             for observation in request.results as! [VNRecognizedTextObservation] {
                 for candiate in observation.topCandidates(1) {
-                    recognizedText.append(candiate.string)
+                    result.append(candiate.string)
                 }
             }
-            if let error = error {
-                callback(.failure(error))
-            } else {
-                callback(.success(recognizedText))
-            }
+            semaphore.signal()
         }
         request.recognitionLanguages = languages
         request.recognitionLevel = .accurate
-        guard let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
-            callback(.failure(NSError(domain: "", code: 0)))
-            return
-        }
         let imageRequestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-        do {
-            try imageRequestHandler.perform([request])
-        } catch {
-            callback(.failure(error))
+        try imageRequestHandler.perform([request])
+        semaphore.wait()
+        if let error = error {
+            throw error
         }
+        return result
     }
 
     mutating func run() throws {
-        if path.isEmpty {
-            print("Please provide a path to an image.")
-            return
-        }
-        guard let absolutePath = realpath(path, nil) else {
-            print("The path \(path) does not exist.")
-            return
-        }
-        let absPath = String(cString: absolutePath)
-        guard let image = NSImage(contentsOfFile: absPath) else {
-            print("Could not load image from path: \(absPath)")
-            return
-        }
-        recognize(image: image, languages: [language]) { result in
-            switch result {
-            case .success(let recognizedText):
-                print(recognizedText.joined(separator: "\n"))
-            case .failure(let error):
-                print(error.localizedDescription)
+        if showSupportedLanguages {
+            if #available(macOS 12.0, *) {
+                print("Supported languages:")
+                let supportedLanguageCodes = try VNRecognizeTextRequest().supportedRecognitionLanguages()
+                for code in supportedLanguageCodes {
+                    let paddedCode = code.padding(toLength: 9, withPad: " ", startingAt: 0)
+                    let locale = Locale(identifier: code)
+                    if let languageName = locale.localizedString(forLanguageCode: code) {
+                        print("\(paddedCode): \(languageName)")
+                    } else {
+                        print(code)
+                    }
+                }
+            } else {
+                throw MyError.notSupported
             }
+            return
         }
+        let path = self.path ?? ""
+        if path.isEmpty {
+            throw MyError.missingPath
+        }
+        guard let absPath = realpath(path, nil) else {
+            throw MyError.invalidPath(path)
+        }
+        let absolutePath = String(cString: absPath)
+        guard let image = NSImage(contentsOfFile: absolutePath) else {
+            throw MyError.couldNotLoadImage(absolutePath)
+        }
+        let recognizedText = try recognize(image: image, languages: languages)
+        print(recognizedText.joined(separator: "\n"))
     }
 }
